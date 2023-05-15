80ea8fec1ff81a2fba1b6a898aaa0fd36ef31d99
wsytcl <43516416+wengsy150943@users.noreply.github.com>
2023-05-15 21:18:59 +0800

    [fix][WIP] align data format

--- cmd/parse.go
+++ cmd/parse.go
@@ -10,18 +10,19 @@ import (
 )
 
 
+
 // 根据查询参数获取结果
-func getResult(QueryPara Query)string {
+func getResult(QueryPara Query)map[string]string {
 	// 正确性验证
 	if QueryPara.month == "" && QueryPara.metric == "" {
 		fmt.Errorf("Lack of enough parameters: either month or metric is required")
-		return ""
+		return nil
 	}
 
 	// 特定指标
 	if QueryPara.month == "" {
-		result, _ := service.Get_On_certain_repo(QueryPara.repo, QueryPara.metric)
-		return string(result)
+		ret, _, _ := service.GetCertainRepo(QueryPara.repo, QueryPara.metric)
+		return ret
 	}
 
 	// 特定月份的整体报告
@@ -30,5 +31,5 @@ func getResult(QueryPara Query)string {
 	}
 
 	// 特定月份在特定指标上的数据
-	return string(service.Get_On_certain_month(QueryPara.repo, QueryPara.metric, QueryPara.month))
+	return service.GetCertainMonth(QueryPara.repo, QueryPara.metric, QueryPara.month)
 }
