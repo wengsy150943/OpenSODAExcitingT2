66cf52298559b43098ea3daaf80afd3a9568413e
lh123cha <48644550+lh123cha@users.noreply.github.com>
2023-06-19 13:37:29 +0800

    add plot line service

--- service/plotservice.go
+++ service/plotservice.go
@@ -25,9 +25,33 @@ func PlotBar(metric string, body []byte) {
 	bar.SetGlobalOptions(charts.TitleOpts{Title: "Bar-示例图"}, charts.ToolboxOpts{Show: true})
 	bar.AddXAxis(monthItems).
 		AddYAxis(metric+"值", numsItems)
-	f, err := os.Create("../assets/images/bar.html")
+	f, err := os.Create("../assets/images/" + metric + "_bar.html")
 	if err != nil {
 		log.Println(err)
 	}
 	bar.Render(f)
+}
+
+func Plotline(metric string, body []byte) {
+	monthItem := []string{}
+	numsItem := []float64{}
+
+	var v interface{}
+	json.Unmarshal(body, &v)
+	data := v.(map[string]interface{})
+
+	for k, v := range data {
+		monthItem = append(monthItem, k)
+		numsItem = append(numsItem, v.(float64))
+	}
+	line := charts.NewLine()
+	line.SetGlobalOptions(charts.TitleOpts{
+		Title: metric + "折线图",
+	}, charts.ToolboxOpts{Show: true})
+	line.AddXAxis(monthItem).AddYAxis(metric+"值", numsItem)
+	f, err := os.Create("../assets/images/" + metric + "_line.html")
+	if err != nil {
+		log.Println(err)
+	}
+	line.Render(f)
 }
--- service/plotservice_test.go
+++ service/plotservice_test.go
@@ -8,3 +8,8 @@ func TestPlotBar(t *testing.T) {
 	_, body, _ := GetCertainRepo("X-lab2017/open-digger", "openrank")
 	PlotBar("openrank", body)
 }
+
+func TestPlotline(t *testing.T) {
+	_, body, _ := GetCertainRepo("X-lab2017/open-digger", "openrank")
+	Plotline("openrank", body)
+}
