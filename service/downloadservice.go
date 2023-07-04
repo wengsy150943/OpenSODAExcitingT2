f90423e94cfacbe85874e84f1041d2d4f530af69
qkgoalkeeper <qiankunhm@163.com>
2023-07-03 19:49:45 -0700

    [style]（downloadservice） 删除了debug时的各种print，删除了注释代码

--- service/downloadservice.go
+++ service/downloadservice.go
@@ -2,17 +2,11 @@ package service
 
 import (
 	"encoding/csv"
-	"fmt"
 	"html/template"
 	"log"
 	"os"
 	"strconv"
 )
-
-//type DownloadService interface {
-//	SetData(source_ RepoInfo, target_ string) error
-//	Download() error
-//}
 
 type SingleDownloadService struct {
 	Source string
@@ -20,7 +14,6 @@ type SingleDownloadService struct {
 	Title  string
 	Dates  []string
 	Data   map[string]([]float32)
-	//Recent map[string]float32
 }
 
 func (d *SingleDownloadService) SetData(source_ RepoInfo, target_ string) error {
@@ -29,39 +22,19 @@ func (d *SingleDownloadService) SetData(source_ RepoInfo, target_ string) error 
 	d.Title = source_.repoName
 
 	d.Dates = source_.dates
-	//dates := []string{"2020-08", "2020-09", "2020-10", "2020-11", "2020-12", "2021-01", "2021-02", "2021-03", "2021-04", "2021-05", "2021-06", "2021-07", "2021-08", "2021-09", "2021-10", "2021-10-raw", "2021-11", "2021-12", "2022-01", "2022-02", "2022-03", "2022-04", "2022-05", "2022-06", "2022-07", "2022-08", "2022-09", "2022-10", "2022-11", "2022-12", "2023-01", "2023-02", "2023-03", "2023-04"}
-	//d.Dates = append(d.Dates, dates...)
-	//openRanks := []float64{4.5, 4.91, 5.59, 6.31, 9.96, 10.61, 6.28, 4.14, 4.44, 4.26, 6.46, 4.84, 3.93, 3.34, 3, 2.84, 2.89, 3.33, 4.71, 4.87, 6.06, 3.76, 4.14, 7.67, 9.17, 8.53, 9.96, 11.84, 14.65, 19.36, 19.9, 40.48, 22.05, 18.79}
 	d.Data = make(map[string]([]float32))
 	for k, v1 := range source_.data {
 		tempList := make([]float32, 0)
 		for _, v2 := range d.Dates {
 			temp, ok := v1[v2]
 			if ok {
-				fmt.Println(v2)
-				fmt.Println(temp)
 				tempList = append(tempList, temp)
 			} else {
 				continue
 			}
 		}
 		d.Data[k] = tempList
-
-		fmt.Println(d.Data[k])
 	}
-
-	//d.Data["openrank"] = openRanks
-	//d.Data["metrics2"] = openRanks
-
-	//d.Recent = make(map[string]float64)
-	//
-	//for i := len(dates) - 1; i >= len(dates)-5; i-- {
-	//	if i < 0 {
-	//		break
-	//	}
-	//	d.Recent[dates[i]] = openRanks[i]
-	//}
-
 	return nil
 }
 
@@ -228,17 +201,6 @@ func (d *CompareDownloadService) SetData(source1_ RepoInfo, source2_ RepoInfo, t
 			continue
 		}
 	}
-
-	//
-	//c := &CompareDownloadData{}
-	//openRanks := []float64{4.5, 4.91, 5.59, 6.31, 9.96, 10.61, 6.28, 4.14, 4.44, 4.26, 6.46, 4.84, 3.93, 3.34, 3, 2.84, 2.89, 3.33, 4.71, 4.87, 6.06, 3.76, 4.14, 7.67, 9.17, 8.53, 9.96, 11.84, 14.65, 19.36, 19.9, 40.48, 22.05, 18.79}
-	//
-	//
-	//c.Data1 = openRanks
-	//c.Data2 = openRanks
-	//d.Data["openrank"] = *c
-	//d.Data["metric2"] = *c
-
 	return nil
 }
 
