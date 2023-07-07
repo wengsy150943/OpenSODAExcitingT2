5bf890e4e689ae45418586273fbd45082f4efb0b
孙印政 <57469177+YinZheng-Sun@users.noreply.github.com>
2023-07-07 14:30:57 +0800

    [Feature][WIP]: batch

--- parse/batch.go
+++ parse/batch.go
@@ -4,8 +4,12 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package parse
 
 import (
-	// "exciting-opendigger/service"
+	"bufio"
+	"exciting-opendigger/service"
 	"github.com/spf13/cobra"
+	"io"
+	"log"
+	"os"
 )
 
 var inputFile string
@@ -21,30 +25,30 @@ var batchCmd = &cobra.Command{
 	Short: "Select a batch of data from OpenDigger",
 	Long:  "Select a batch of data from OpenDigger",
 	Run: func(cmd *cobra.Command, args []string) {
-		// 看一下是检索top的还是从文件检索
-		// ifTop := inputFile == "TOP"
-		
-		// var repoList []string
-		// if (ifTop){
-		// 	// TODO qk: call crawler
-		// }else{
-		// 	// TODO syz lh: read file
-		// }
+		//看一下是检索top的还是从文件检索
+		ifTop := inputFile == "TOP"
 
-		// // TODO syz lh: get repo info
-		// repoInfoList := service.GetRepoList(repoList)
-
-
-		// var downloadService service.DownloadService
-		// downloadService = &service.BatchDownloadService{}
-		// downloadService.SetData(repoInfoList, outputFile)
-		// downloadService.Download()
+		var repoList []string
+		if ifTop {
+			// TODO qk: call crawler
+		} else {
+			file, e := os.Open(inputFile)
+			if e != nil {
+				log.Fatalf(inputFile + " file not found!")
+			}
+			defer file.Close()
+			buffer := bufio.NewReader(file)
+			for {
+				repo, _, e := buffer.ReadLine()
+				if e == io.EOF {
+					break
+				}
+				repoList = append(repoList, string(repo))
+			}
+		}
+		service.DownLoadRepoList(repoList, outputFile)
 	},
 }
-
-
-
-
 
 func init() {
 	rootCmd.AddCommand(batchCmd)
@@ -53,13 +57,11 @@ func init() {
 	batchCmd.Flags().StringVarP(&outputFile, "position", "p", "", "Repo output file")
 	batchCmd.Flags().StringVarP(&filter, "filter", "f", "", "Filter for metric")
 	batchCmd.Flags().StringVarP(&sortKey, "orderBy", "o", "", "Sort key")
-	batchCmd.Flags().BoolVar(&asc,"asc",true,"Sort ASC")
-	batchCmd.Flags().BoolVar(&desc,"desc",false,"Sort DESC")
+	batchCmd.Flags().BoolVar(&asc, "asc", true, "Sort ASC")
+	batchCmd.Flags().BoolVar(&desc, "desc", false, "Sort DESC")
 
-	
 	batchCmd.MarkFlagFilename("position")
 	batchCmd.MarkFlagRequired("source")
 	batchCmd.MarkFlagRequired("position")
-	batchCmd.MarkFlagsMutuallyExclusive("asc","desc")
-
+	batchCmd.MarkFlagsMutuallyExclusive("asc", "desc")
 }
