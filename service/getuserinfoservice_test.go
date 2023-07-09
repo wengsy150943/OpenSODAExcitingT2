1ad96dbc0dce6de16f5ca9a2e6fd88f0ced510a4
lh123cha <48644550+lh123cha@users.noreply.github.com>
2023-07-09 19:48:22 +0800

    [feat]:添加查询用户信息功能

--- service/getuserinfoservice_test.go
+++ service/getuserinfoservice_test.go
@@ -3,13 +3,11 @@ package service
 import "testing"
 
 func TestGetCertainUser(t *testing.T) {
-	testData := make(map[string]interface{})
-
 	username := "frank-zsy"
 	a := GetCertainUser(username)
 	println(a.Data["openrank"]["2015-02"].(float64))
-	println(a.Data["developernetwork"]["node"].([]interface{})[0].([]interface{})[0].(string))
-	if a.Data["openrank"]["2015-02"].(float64) != 0.64 || a.Data["developernetwork"]["node"].([]interface{})[0].([]interface{})[0].(string) != "snyk-bot" || a.Data["developernetwork"]["node"].([]interface{})[0].([]interface{})[0].(float64) != 10833.52 {
-
+	println(a.Data["developernetwork"]["nodes"].([]interface{})[0].([]interface{})[0].(string))
+	if a.Data["openrank"]["2015-02"].(float64) != 0.64 || a.Data["developernetwork"]["nodes"].([]interface{})[0].([]interface{})[0].(string) != "snyk-bot" || a.Data["developernetwork"]["nodes"].([]interface{})[0].([]interface{})[1].(float64) != 10833.52 {
+		t.Errorf("Get userinfo failed")
 	}
 }
