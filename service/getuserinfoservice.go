4dcc332a482e2b510c75dd4f53dd86d3e9181b6b
lh123cha <48644550+lh123cha@users.noreply.github.com>
2023-07-09 19:46:01 +0800

    [feat]:添加查询用户信息功能

--- service/getuserinfoservice.go
+++ service/getuserinfoservice.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"sync"
 )
 
 type UserInfo struct {
@@ -11,7 +12,7 @@ type UserInfo struct {
 	Data     map[string](map[string]interface{})
 }
 
-func GetUrlContent(url, username, metric string) UserInfo {
+func GetUserUrlContent(url, username string) map[string]interface{} {
 	response, err := http.Get(url)
 	if err != nil {
 		panic(err)
@@ -20,40 +21,66 @@ func GetUrlContent(url, username, metric string) UserInfo {
 
 	// 解析数据
 	body, _ := ioutil.ReadAll(response.Body)
-
 	var temp map[string]interface{}
-	data_list := map[string]interface{}{}
 	json.Unmarshal([]byte(body), &temp)
-
-	//用户信息返回的结构类型为：map[string][][]sting{}
-	// 获取日期并排序, 需要针对特殊情况做处理
-	if metric == "developernetwork" || metric == "reponetwork" {
-		nodesvalue, ok := temp["nodes"].([][]string)
-		if !ok {
-			panic("cannot convert temp to [][]string")
-		}
-		edgesvalue, _ := temp["edges"].([][]string)
-		data_list["nodes"] = nodesvalue
-		data_list["edges"] = edgesvalue
-	} else {
-		data_list = temp
-	}
-	// 将数据赋值给RepoInfo，如果数据是9种特殊指标，解析为specialData；并赋给data
-	// 获取特殊指标对应的解析函数
-	var data map[string](map[string]interface{})
-	data = make(map[string](map[string]interface{}))
-	data[metric] = data_list
-	ret := UserInfo{
-		Username: username,
-		Data:     data,
-	}
-
-	return ret
+	return temp
 }
-func GetCertainUser(username string) {
+func appendlist(wg *sync.WaitGroup, m *sync.Mutex, l map[string](map[string]interface{}), t map[string]interface{}, metric string) {
+	m.Lock()
+	l[metric] = t
+	m.Unlock()
+	wg.Done()
+}
+func GetCertainUser(username string) UserInfo {
 	//TODO 获得四个指标的url，开四个携程分别去请求url，结果写入UserInfo
 	BaseURL := "https://oss.x-lab.info/open_digger/github/"
 	openrankURL := BaseURL + username + "/" + "openrank.json"
+	activity := BaseURL + username + "/" + "activity.json"
 	developernetworkURL := BaseURL + username + "/" + "developer_network.json"
-	GetUrlCotent(openrankURL)
+	reponetwork := BaseURL + username + "/" + "repo_network.json"
+	ret := UserInfo{}
+	ret.Username = username
+	Urls := []string{openrankURL, activity, developernetworkURL, reponetwork}
+	res := map[string](map[string]interface{}){}
+	UserMetric := []string{"openrank", "activity", "developernetwork", "reponetwork"}
+
+	var wg sync.WaitGroup
+	var m sync.Mutex
+	for i := 0; i < 4; i++ {
+		wg.Add(1)
+		go appendlist(&wg, &m, res, GetUserUrlContent(Urls[i], username), UserMetric[i])
+	}
+	wg.Wait()
+	//println(res["developernetwork"]["nodes"].([]interface{})[0].([]interface{})[0].(string))
+	//println(res["reponetwork"]["nodes"].([]interface{})[0].([]interface{})[0].(string))
+	//map赋值前需要先初始化(包括内部的map[string]interface{}
+	data := make(map[string](map[string]interface{}))
+	data["developernetwork"] = make(map[string]interface{})
+	data["reponetwork"] = make(map[string]interface{})
+
+	//包括内部的map[string]interface{}
+	data_list := make(map[string]interface{})
+	data_list_developerinfo := make(map[string]interface{})
+	for k, _ := range res {
+		//=使用浅赋值，data[k]指向data_list的地址，需要重新深拷贝
+		if k == "openrank" || k == "activity" {
+			data_list = res[k]
+			data[k] = data_list
+		} else if k == "reponetwork" {
+			//println(k)
+			data_list["nodes"] = res[k]["nodes"]
+			data_list["edges"] = res[k]["edges"]
+			data[k] = data_list
+		} else {
+			data_list_developerinfo["nodes"] = res[k]["nodes"]
+			data_list_developerinfo["edges"] = res[k]["edges"]
+			data[k] = data_list_developerinfo
+		}
+
+	}
+	ret.Data = data
+
+	//println(ret.Data["developernetwork"]["nodes"].([]interface{})[0].([]interface{})[0].(string))
+	//println(ret.Data["reponetwork"]["nodes"].([]interface{})[0].([]interface{})[0].(string))
+	return ret
 }
